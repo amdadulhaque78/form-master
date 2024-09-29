03a7ad77ccce0bdf81246b25075fb5161a3545e0
amdadulhaque78 <amdadulhaque.ier.du@gmail.com>
2024-09-29 12:34:57 +0600

    access form data using onSubmit, explore useRef and explore controlled and uncontrolled element

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import RefForm from './components/RefForm/RefForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
+// import StateFulForm from './components/StateFulForm/StateFulForm'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StateFulForm></StateFulForm> */}
+      <RefForm></RefForm>
+      
+    </>
+  )
+}
+
+export default App
